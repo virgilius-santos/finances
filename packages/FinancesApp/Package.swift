19ccd2e9b6a39ef6f1702c0bb06d5c4265fd107f
virgilius-santos <virgilius.santos@acad.pucrs.br>
2023-11-28 07:57:23 -0300

    add package dependency

--- packages/FinancesApp/Package.swift
+++ packages/FinancesApp/Package.swift
@@ -12,17 +12,20 @@ let package = Package(
             targets: ["FinancesApp"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/nalexn/ViewInspector", from: "0.9.8"),
+         .package(name: "FinancesCoreSharedTests", path: "FinancesCore"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FinancesApp",
             dependencies: []),
         .testTarget(
             name: "FinancesAppTests",
-            dependencies: ["FinancesApp"]),
+            dependencies: [
+                "FinancesApp",
+                "ViewInspector",
+                "FinancesCoreSharedTests",
+            ]
+        ),
     ]
 )
