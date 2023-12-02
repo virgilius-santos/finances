85d946436311abc8b2b257eb22705d3b88440dd5
virgilius-santos <virgilius.santos@acad.pucrs.br>
2023-12-02 15:44:26 -0300

    move extension to own file

--- packages/FinancesApp/Tests/FinancesAppTests/SheetsViewTests.swift
+++ packages/FinancesApp/Tests/FinancesAppTests/SheetsViewTests.swift
@@ -274,33 +274,3 @@ private extension SheetsViewTests.Doubles {
         store.getSheetsCompletion?()
     }
 }
-
-extension View {
-    func tap(on value: String) throws {
-        try inspect()
-            .find(button: "Add Sheet")
-            .tap()
-    }
-    
-    func findValue(for id: String) throws -> String {
-        try inspect()
-        .find(viewWithAccessibilityIdentifier: "EmptyState.Text")
-        .text()
-        .string()
-    }
-    
-    func getListRowsIds(numberOfRows: Int) throws -> [String] {
-        var rowsIds = [String]()
-        let list = try inspect()
-            .find(viewWithAccessibilityIdentifier: "List.full")
-            .list()
-        for i in 0..<numberOfRows {
-            try rowsIds.append(list.forEach(0).text(i).string())
-        }
-        return rowsIds
-    }
-    
-    func callOnAppear() throws {
-        try inspect().group().callOnAppear()
-    }
-}
---
+++ packages/FinancesApp/Tests/FinancesAppTests/ViewInspectorExtensions.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+import ViewInspector
+import XCTest
+
+extension View {
+    func tap(on value: String) throws {
+        try inspect()
+            .find(button: "Add Sheet")
+            .tap()
+    }
+    
+    func findValue(for id: String) throws -> String {
+        try inspect()
+        .find(viewWithAccessibilityIdentifier: "EmptyState.Text")
+        .text()
+        .string()
+    }
+    
+    func getListRowsIds(numberOfRows: Int, file: StaticString = #filePath, line: UInt = #line) throws -> [String] {
+        var rowsIds = [String]()
+        let list = try inspect()
+            .find(viewWithAccessibilityIdentifier: "List.full")
+            .list()
+        for i in 0..<numberOfRows {
+            try rowsIds.append(list.forEach(0).text(i).string())
+        }
+        XCTAssertThrowsError(try list.forEach(0).text(numberOfRows), "invalid number of elements", file: file, line: line)
+        return rowsIds
+    }
+    
+    func callOnAppear() throws {
+        try inspect().group().callOnAppear()
+    }
+}
