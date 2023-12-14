8b446af65047f7fbb3193d4464e4d40c179eafa0
virgiliu santos <virgilius_santos@yahoo.com.br>
2023-12-13 21:46:52 -0300

    add search

--- packages/FinancesApp/Sources/FinancesApp/AppPromo/AppPromoView.swift
+++ packages/FinancesApp/Sources/FinancesApp/AppPromo/AppPromoView.swift
@@ -16,7 +16,7 @@ public struct AppPromo: View {
     @AppStorage("isAppLockEnabled") var isAppLockEnabled = false
     @AppStorage("lockWhenApppGoesBackground") var lockWhenApppGoesBackground = false
     
-    @State private var activeTab = Tab.Item.recents
+    @State private var activeTab = Tab.Item.search
     
     public var body: some View {
         LockView(
@@ -360,10 +360,36 @@ extension AppPromo.Recents {
     }
 }
 
+import Combine
+
 extension AppPromo.Search {
     struct SearchView: View {
+        @State private var searchText = ""
+        @State private var filterText = ""
+        
+        private let searchPublisher = PassthroughSubject<String, Never>()
+        
         var body: some View {
-            Text("Search")
+            NavigationStack {
+                ScrollView(.vertical) {
+                    LazyVStack(spacing: 12) {
+                        
+                    }
+                }
+                .overlay(content: {
+                    ContentUnavailableView("Search Transactions", systemImage: "magnifyingglass")
+                        .opacity(filterText.isEmpty ? 1 : 0)
+                })
+                .onChange(of: searchText, { oldValue, newValue in
+                    searchPublisher.send(newValue)
+                })
+                .onReceive(searchPublisher.debounce(for: .seconds(0.4), scheduler: DispatchQueue.main), perform: { text in
+                    filterText = text
+                })
+                .searchable(text: $searchText)
+                .navigationTitle("Search")
+                .background(.gray.opacity(0.15))
+            }
         }
     }
 }
