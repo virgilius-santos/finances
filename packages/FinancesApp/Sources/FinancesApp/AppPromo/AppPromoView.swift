915529a327ba411a9aa2567e5932ca8d24abf3aa
virgiliu santos <virgilius_santos@yahoo.com.br>
2023-12-13 21:35:24 -0300

    finish lock screen

--- packages/FinancesApp/Sources/FinancesApp/AppPromo/AppPromoView.swift
+++ packages/FinancesApp/Sources/FinancesApp/AppPromo/AppPromoView.swift
@@ -13,21 +13,30 @@ public extension WindowGroup where Content == AppPromo {
 
 public struct AppPromo: View {
     @AppStorage("isFirstTime") private var isFirstTime = true
+    @AppStorage("isAppLockEnabled") var isAppLockEnabled = false
+    @AppStorage("lockWhenApppGoesBackground") var lockWhenApppGoesBackground = false
+    
     @State private var activeTab = Tab.Item.recents
     
     public var body: some View {
-        TabView(selection: $activeTab) {
-            ForEach(Tab.Item.allCases, id: \.self) { tab in
-                tab.view
-                    .tag(tab)
-                    .tabItem { tab.content }
-            }
-        }
-        .tint(Color.appTint)
-        .sheet(isPresented: $isFirstTime, content: {
-            AppPromo.IntroScreen.Home()
-                .interactiveDismissDisabled()
-        })
+        LockView(
+            lockType: .both, lockPin: "1234", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenApppGoesBackground,
+            content: {
+                TabView(selection: $activeTab) {
+                    ForEach(Tab.Item.allCases, id: \.self) { tab in
+                        tab.view
+                            .tag(tab)
+                            .tabItem { tab.content }
+                    }
+                }
+                .tint(Color.appTint)
+                .sheet(isPresented: $isFirstTime, content: {
+                    AppPromo.IntroScreen.Home()
+                        .interactiveDismissDisabled()
+                })
+            },
+            forgotPin: {}
+        )
     }
     
     public init() {}
